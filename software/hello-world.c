6ea739282dfcb72a847db71c9519a48070c2b872
Lars Juhl <larsuldalljuhl@gmail.com>
2018-12-03 09:55:38 +0100

    Opdateret

---
+++ software/hello-world.c
@@ -0,0 +1,13 @@
+ // Lille testprogram 
+
+#include <stdio.h>
+
+int main () {
+
+	printf("\n Hello World and Team3 \n");
+
+	return 0;
+
+
+	// For at køre programmet i terminalen skriv: ./HelloProgram
+}
